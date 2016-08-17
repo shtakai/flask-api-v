53ff0f20c7b4d1831745fe6a50037de8ccfb0542
shtakai <shtakai@gmail.com>
2016-08-17 17:48:20 +0900

    implemented

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import random
+
+from quotes import funny_quotes
+
+
+app = Flask(__name__)
+
+
+@app.route("/api/funny")
+def serve_funny_quote():
+    quotes = funny_quotes()
+    nr_of_quotes = len(quotes)
+    selected_quote = quotes[random.randint(0, nr_of_quotes - 1)]
+    return jsonify(selected_quote)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
